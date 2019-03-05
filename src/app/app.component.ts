670d3c78ea230c7a55c5c86f6232730e652df83e
ngaitan98 <n.gaitan@uniandes.edu.co>
2019-03-04 22:40:00 -0500

    Finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bono1Parcial';
+  newDone: string = "";
+  newTodo: string = "";
+  newDoing: string = "";
+
+  todoList: string[] = ['Estudiar Web', 'Hacer taller de MOS', 'Finalizar Entrega del CBU'];
+  doingList: string[] = ['Hacer bono Web', 'Revisar Entrega Master Card'];
+  doneList: string[] = ['Entregar MOS', 'Leer para el CBU', 'Estudiar SISCONGER', 'Hacer entrega Web'];
+
+  addDone(){
+    this.doneList.push(this.newDone);
+  }
+  addTodo(){
+    this.todoList.push(this.newTodo);
+  }
+  addDoing(){
+    this.doingList.push(this.newDoing);
+  }
+  drop(event: CdkDragDrop<string[]>){
+    if(event.previousContainer !== event.container){
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
+    else{
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
